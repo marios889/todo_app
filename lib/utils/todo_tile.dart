e97d444f1d071698e84a9e82c3cc34d8bfe3124d
mariosmagid <mariosmagid@gmail.com>
2026-07-21 23:43:05 +0300

    added a tile reusable widget that takes its name and if task is completed and the checkbox on changed behaviour with changing the style of the text with the checkbox value for if task is completed or not

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   void Function(bool?)? onChanged;
+  void Function(BuildContext)? deleteFunction;
   TodoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
+      padding: EdgeInsets.only(left: 25.0, right: 25, top: 25),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-            Text(taskName),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade300,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  fontWeight: taskCompleted
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
